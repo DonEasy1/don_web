303451f872482490d0fa923d7ac7c49bbdaeefee
Don <doneasy1@gmail.com>
2019-05-08 15:01:30 +0000

    tightened email validation

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,5 +1,5 @@
 class Contact < ActiveRecord::Base
   validates :name, presence: true
-  validates :email, presence: true
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
   validates :comments, presence: true
 end
