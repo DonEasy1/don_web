6c6f4160f260c607bb85e99598fcbb2aac9b03ee
Don <doneasy1@gmail.com>
2019-05-08 14:17:28 +0000

    added contact create action to save to db

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,13 +1,23 @@
 class ContactsController < ApplicationController
+  
+# GET /contact-us  
+# Show new contact form
   def new
     @contact = Contact.new
   end
-
+  
+# POST request /contacts
 def create
+  # mass assignment of form fields into Contact object
   @contact = Contact.new(contact_params)
+  
+# save contact object to database
   if @contact.save
+    # show message and redirect to new action
     redirect_to new_contact_path, notice: "Message sent."
   else
+    # if contact object no save:
+    # show message and redirect to new action/form page
     redirect_to new_contact_path, notice: "Error occurred."
   end
 end
