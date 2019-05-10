bd0230aa98fa73c0fcabb3f62c63d9560bd998c8
Don <doneasy1@gmail.com>
2019-05-10 16:56:25 +0000

    added comments to contact-related files

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -10,24 +10,31 @@ class ContactsController < ApplicationController
 def create
   # mass assignment of form fields into Contact object
   @contact = Contact.new(contact_params)
+  # request to poste to database
   if @contact.save
+  # store forme fields via parameters into variables
     name = params[:contact][:name]
     email = params[:contact][:email]
     body = params[:contact][:comments]
+    # Plug variables into Contact Mailer
+    # Email method and send email
     ContactMailer.contact_email(name, email, body).deliver
+    # Stire syccess nessage ub flash hash.
     flash[:success] = "Message sent."
     # show message and redirect to new action
     redirect_to new_contact_path
   else
+    # If contact no save,
+    # store errors in flash hash
     flash[:danger] = @contact.errors.full_messages.join(",")
-    # if contact object no save:
-    # show message and redirect to new action/form page
+    # redirect to new action/form page
     redirect_to new_contact_path
   end
 end
 
 # whitelist this contact form input
 private 
+  #strong parameters
   def contact_params
     params.require(:contact).permit(:name, :email, :comments)
   end
--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,4 +1,5 @@
 class Contact < ActiveRecord::Base
+  #contact form validations
   validates :name, presence: true
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
   validates :comments, presence: true
