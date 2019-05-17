ac5496a690a3e7d74f7919b0c966fcb0538fd263
Don <doneasy1@gmail.com>
2019-05-17 20:33:28 +0000

    Added user profiles form

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -2,6 +2,8 @@ class ProfilesController < ApplicationController
   
   # GET to /users/:user_id/profile/new(
   def new
+    @profile = Profile.new
+    # @plan = User.find(current_user.id).plan_id (<=experimenting here)
     # render blank profile details form, so can enter info and save and then create, which means can make corresponding view file)
   end
 end
