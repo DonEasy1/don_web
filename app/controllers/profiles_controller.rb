1f874d27fdff33209097f255f6a0ed425223220b
Don <doneasy1@gmail.com>
2019-05-17 17:06:03 +0000

    added profiles controller, new view with link on connect page

---
+++ app/controllers/profiles_controller.rb
@@ -0,0 +1,7 @@
+class ProfilesController < ApplicationController
+  
+  # GET to /users/:user_id/profile/new(
+  def new
+    # render blank profile details form, so can enter info and save and then create, which means can make corresponding view file)
+  end
+end
