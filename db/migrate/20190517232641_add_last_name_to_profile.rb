9c53a0be3cfff161ec0f4e85b6cdd677e8409c9c
Don <doneasy1@gmail.com>
2019-05-18 00:31:07 +0000

    added/fixed 2 Profiles Table columns & added field to profile form

---
+++ db/migrate/20190517232641_add_last_name_to_profile.rb
@@ -0,0 +1,5 @@
+class AddLastNameToProfile < ActiveRecord::Migration[5.0]
+  def change
+    add_column :profiles, :first_name, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190517050017) do
+ActiveRecord::Schema.define(version: 20190517235143) do
 
   create_table "contacts", force: :cascade do |t|
     t.string   "name"
@@ -37,6 +37,8 @@ ActiveRecord::Schema.define(version: 20190517050017) do
     t.string   "description"
     t.datetime "created_at",           null: false
     t.datetime "updated_at",           null: false
+    t.string   "first_name"
+    t.string   "role"
   end
 
   create_table "users", force: :cascade do |t|
